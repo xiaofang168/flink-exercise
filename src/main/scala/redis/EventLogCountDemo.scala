7f85055f54720e4c4f0a83f294cc3d799ab840a3
fangjie <fangjie@etouch.cn>
2020-04-07 19:08:47 +0800

    使用rocksdb持久化checkpoint

--- src/main/scala/redis/EventLogCountDemo.scala
+++ src/main/scala/redis/EventLogCountDemo.scala
@@ -1,13 +1,21 @@
 package redis
 
 import mate.EventLog
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
+import org.apache.flink.streaming.api.CheckpointingMode
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
 
 object EventLogCountDemo {
 
+  val dbPath: String = "/Users/fangjie/rocksdb-data/"
+
   def main(args: Array[String]): Unit = {
     // 构造环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    // 开启checkpoint功能
+    env.enableCheckpointing(3000, CheckpointingMode.EXACTLY_ONCE)
+    env.setStateBackend(new RocksDBStateBackend(dbPath, true))
 
     // 获取数据流
     val redisDataStream: DataStream[Set[EventLog]] = env.addSource(new RedisQueueSource())
