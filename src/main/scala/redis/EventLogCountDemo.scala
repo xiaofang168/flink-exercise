c739f1d5a00ed68e3990f603b7f2affc6eb918f8
fangjie <fangjie@etouch.cn>
2020-04-09 21:22:02 +0800

    替换try with resources 依赖

--- src/main/scala/redis/EventLogCountDemo.scala
+++ src/main/scala/redis/EventLogCountDemo.scala
@@ -1,35 +1,50 @@
 package redis
 
 import mate.EventLog
+import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
-import org.apache.flink.streaming.api.CheckpointingMode
+import org.apache.flink.streaming.api.environment.CheckpointConfig
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
 
 object EventLogCountDemo {
 
-  val dbPath: String = "/Users/fangjie/rocksdb-data/"
+  val dbPath: String = "file:///Users/fangjie/rocksdb-data/"
 
   def main(args: Array[String]): Unit = {
     // 构造环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
-    // 开启checkpoint功能
-    env.enableCheckpointing(3000, CheckpointingMode.EXACTLY_ONCE)
-    env.setStateBackend(new RocksDBStateBackend(dbPath, true))
+    val config = env.getCheckpointConfig
+    // set mode to exactly-once (this is the default)
+    config.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+    // 退出不删除checkpoint
+    config.enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
+
+    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+    env.setStateBackend(new RocksDBStateBackend(dbPath, false))
+    // start a checkpoint every 2seconds
+    env.enableCheckpointing(2000)
+    // 重启策略
+    env.setRestartStrategy(RestartStrategies.noRestart())
 
     // 获取数据流
     val redisDataStream: DataStream[Set[EventLog]] = env.addSource(new RedisQueueSource())
+      .name("redis-data-source")
 
     // 数据转换Transformation
-    val result = redisDataStream.flatMap(e => e)
+    val result: DataStream[(Long, Int)] = redisDataStream.flatMap(e => e)
       .map(e => (e.uid, 1))
       .keyBy(0)
       .sum(1)
+      .name("data-count")
 
-    result.print()
+    // 数据输出
+    result
+      .addSink(new SumSink)
+      .name("send-result")
 
-    env.execute("read data from redis")
-
+    env.execute("read-data-from-redis")
   }
 
 }
