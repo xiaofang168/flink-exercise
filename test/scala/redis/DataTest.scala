d54ec7bdd5044e30fca36f125325dfc13b2defd0
fangjie <fangjie@etouch.cn>
2020-04-09 11:22:29 +0800

    增加try with resources 依赖

--- test/scala/redis/DataTest.scala
+++ test/scala/redis/DataTest.scala
@@ -1,10 +1,11 @@
 package redis
 
+import io.tmos.arm.ArmMethods._
 import org.junit.Test
 import org.rocksdb.{Options, RocksDB}
-import util.Helper._
 
 class DataTest {
+
 
   @Test
   def rocksdb(): Unit = {
@@ -12,41 +13,41 @@ class DataTest {
 
     RocksDB.loadLibrary()
 
-    autoClose(new Options().setCreateIfMissing(true)) { options =>
+    // use scala-arm managing resources, try with resources
+    for {
+      options <- manage(new Options().setCreateIfMissing(true))
+      rocksDB <- manage(RocksDB.open(options, dbPath))} {
 
-      autoClose(RocksDB.open(options, dbPath)) { rocksDB =>
-        val key = "Hello".getBytes
-        rocksDB.put(key, "World".getBytes)
-        println(new String(key) + ":" + new String(rocksDB.get(key)))
+      val key = "Hello".getBytes
+      rocksDB.put(key, "World".getBytes)
+      println(new String(key) + ":" + new String(rocksDB.get(key)))
 
-        rocksDB.put("SecondKey".getBytes, "SecondValue".getBytes)
+      rocksDB.put("SecondKey".getBytes, "SecondValue".getBytes)
 
-        val keys = java.util.Arrays.asList(key, "SecondKey".getBytes, "missKey".getBytes)
-        val values = rocksDB.multiGetAsList(keys)
+      val keys = java.util.Arrays.asList(key, "SecondKey".getBytes, "missKey".getBytes)
+      val values = rocksDB.multiGetAsList(keys)
 
-        for (i <- 0 to keys.size() - 1) {
-          // s插值操作, s实际上是一个方法，除了s还有f，raw（不会对字符串进行转义）
-          println(s"multiGet ${new String(keys.get(i))}: ${if (values.get(i) != null) new String(values.get(i)) else null}")
-        }
-
-        // 打印全部[key - value]
-        var iter = rocksDB.newIterator
-        iter.seekToFirst()
-        while (iter.isValid) {
-          println(s"iterator key: ${new String(iter.key)}, iter value: ${new String(iter.value)}")
-          iter.next()
-        }
-        // 删除一个key
-        rocksDB.delete(key)
-        println(s"after remove key:${new String(key)}")
-        iter = rocksDB.newIterator
-        iter.seekToFirst()
-        while (iter.isValid) {
-          println(s"iterator key: ${new String(iter.key)}, iter value:${new String(iter.value)}")
-          iter.next()
-        }
+      for (i <- 0 to keys.size() - 1) {
+        // s插值操作, s实际上是一个方法，除了s还有f，raw（不会对字符串进行转义）
+        println(s"multiGet ${new String(keys.get(i))}: ${if (values.get(i) != null) new String(values.get(i)) else null}")
       }
 
+      // 打印全部[key - value]
+      var iter = rocksDB.newIterator
+      iter.seekToFirst()
+      while (iter.isValid) {
+        println(s"iterator key: ${new String(iter.key)}, iter value: ${new String(iter.value)}")
+        iter.next()
+      }
+      // 删除一个key
+      rocksDB.delete(key)
+      println(s"after remove key:${new String(key)}")
+      iter = rocksDB.newIterator
+      iter.seekToFirst()
+      while (iter.isValid) {
+        println(s"iterator key: ${new String(iter.key)}, iter value:${new String(iter.value)}")
+        iter.next()
+      }
     }
   }
 
